984bf18793f33ba00fd4aa810fb5eab5f615ca1c
Chaitanya Mittal <chtnnh@gmail.com>
2020-10-30 23:11:44 +0400

    Remove namespace option in nlwiki_template_extractor.py for testing purposes

--- nlwiki_articlequality/nlwiki_template_extractor.py
+++ nlwiki_articlequality/nlwiki_template_extractor.py
@@ -7,13 +7,12 @@ the mwreverts libraray
 USAGE:
     nlwiki_template_extractor (-h|--help)
     nlwiki_template_extractor <xml-dump>... 
-        [--namespace=<num>...] [--processes=<num>]
+        [--processes=<num>]
         [--output=<path>]
 
 OPTIONS
     -h --help   Print this documentation
     <xml-dump>  Path to an XML dump to process (could be compressed gzip, bz2, etc.)
-    --namespace=<num>  MediaWiki namespaces to include in processing [default: <all_namespaces>]
     --processes=<num>  Number of parallel processes to use for extraction [default: <cpu_count>]
     --output=<path>    Where to write the output file [default: <stdout>]
 
@@ -37,24 +36,17 @@ def main():
     else:
         threads = int(args['--processes'])
     
-    if args['--namespaces'] == "<all_namespaces>":
-        namespaces = None
-    else:
-        namespaces = set(int(v) for v in args['--namespaces'])
-    
     if args['--output'] == "<stdout>":
         output = sys.stdout
     else:
         output = open(args['--output'], "w")
     
-    run(paths, threads, namespaces, output)
+    run(paths, threads, output)
 
-def run(paths, threads, namespaces, output):
+def run(paths, threads, output):
     
     def process_template_changes(dump, path):
         for page in dump:
-            if namespaces is not None and page.namespace not in namespaces:
-                continue
             template_appeared = False
             for revision in page:
                 if not template_appeared and TEMPLATE_RE.search(revision.text): #TODO: Make sure this evaluated to False when we don't find anything
